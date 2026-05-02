180445469f24810cb8fd6358b2bd12301625c319
William Flaitt <wksflaitt@gmail.com>
2026-05-02 20:12:16 -0300

    Primeiro oPrograsma

--- exercicios/src/fundamentos/PrimeiroPrograma1.java
+++ exercicios/src/fundamentos/PrimeiroPrograma1.java
@@ -6,17 +6,26 @@ package fundamentos;
  * @since JDK1.0
  */
 public class PrimeiroPrograma1 {
-
-	/**
-	 * ...
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
+    public static void main(String[] args) {
+        System.out.println("Número de argumentos: " + args.length);
 		// Sentença de código termina com ;
-		// Mais uma linha...
+		// Mais uma linha... 
+		// Precisa estudar a biblioteca System.out.
 		// Fim
 		System.out.println("Primeiro programa Parte #01!");
+		System.err.println("tesrte + " );
+		
+		/*
+		 * Linha 1
+		 * Linha 2
+		 * Linha 3
+		 */
+		System.out.println("Primeiro programa Parte #02!");		// Sentença de código termina com ;
+		// Mais uma linha... 
+		// Precisa estudar a biblioteca System.out.
+		// Fim
+		System.out.println("Primeiro programa Parte #01!");
+		System.err.println("tesrte + " );
 		
 		/*
 		 * Linha 1
@@ -24,5 +33,10 @@ public class PrimeiroPrograma1 {
 		 * Linha 3
 		 */
 		System.out.println("Primeiro programa Parte #02!");
-	}
+		
+        for(int i = 0; i < args.length; i++) {
+            System.out.println("args[" + i + "] = " + args[i]);
+        }
+    }
+    
 }
